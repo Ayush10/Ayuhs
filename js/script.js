43f793442c9b7115ee3b05dcd5ee764e7c4b9c74
Ayush Ojha <ayushgreat10@gmail.com>
2020-09-03 23:54:57 +0545

    fourth commit

---
+++ js/script.js
@@ -0,0 +1,88 @@
+
+
+// Portfolio Item Filter
+
+const FilterContainer = document.querySelector(".portfolio-filter"),
+    filterBtns = filterContainer.children,
+    totalFilterBtn = filterBtns.length;
+    portfolioItems = document.querySelectorAll(".portfolio-item"),
+    totalPortfolioItem= portfolioItems.length;
+
+    for (let i = 0; i < totalFilterBtn; i++) {
+        filterBtns[i].addEventListener("click", function () {
+            filterBtns[i].querySelector(".active").classList.remove("active");
+            this.classList.add("active");
+
+            const filterValue = this.getAttribute("data-filter");
+            for (let k = 0; k < totalPortfolioItem; k++) {
+                if (filterValue === portfolioItems[k].getAttribute("data-category")) {
+                    portfolioItems.classList[k].remove("hide");
+                    portfolioItems.classList[k].add("show");
+                }
+                else {
+                    portfolioItems.classList[k].remove("show");
+                    portfolioItems.classList[k].add("hide");
+                }
+                if (filterValue === "all") {
+                    portfolioItems.classList[k].remove("hide");
+                    portfolioItems.classList[k].add("show");
+                }
+            }
+        })
+
+    }
+
+// Portfolio Light Box
+const lightbox = document.querySelector(".lightbox"),
+    lightboxImg = lightbox.querySelector("lightbox-img"),
+    lightboxClose = lightbox.querySelector(".lightbox-close"),
+    ligtboxText = lightbox.querySelector(".caption-text"),
+    lightboxCounter = lightbox.querySelector(".caption-counter");
+
+    let itemIndex = 0;
+
+    for (let i = 0; i < totalPortfolioItem; i++) {
+        portfolioItems[i].addEventListener("click", function () {
+            itemIndex = i;
+            changeItem();
+            toggleLightbox();
+        })
+    }
+
+    function nextItem() {
+        if (itemIndex === totalPortfolioItem - 1) {
+            itemIndex = 0;
+        }
+        else {
+            itemIndex++;
+        }
+        changeItem();
+    }
+
+function prevItem() {
+    if (itemIndex === 0) {
+        itemIndex = totalPortfolioItem-1;
+    }
+    else {
+        itemIndex--;
+    }
+    changeItem();
+}
+
+    function toggleLightbox() {
+        lightbox.classList.toggle("open");
+    }
+
+    function changeItem() {
+        imgSrc = portfolioItems[itemIndex].querySelector(".portfolio-img img").getAttribute("src");
+        lightboxImg.src = imgSrc;
+        ligtboxText.innerHTML = portfolioItems[itemIndex].querySelector("h4").innerHTML;
+        lightboxCounter.innerHTML = (itemIndex + 1) + " of " +totalPortfolioItem;
+    }
+
+    // close Light Box
+lightbox.addEventListener("click", function (event) {
+    if (event.target === lightboxClose || event.target === lightbox) {
+        toggleLightbox();
+    }
+})
